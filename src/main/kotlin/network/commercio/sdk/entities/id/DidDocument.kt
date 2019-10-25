4be4dce461423e1050ce52eff1086a433e1ff443
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2019-10-25 13:42:48 +0200

    Fixed an error inside the JSON encoding of the MsgSetDidDocument

--- src/main/kotlin/network/commercio/sdk/entities/id/DidDocument.kt
+++ src/main/kotlin/network/commercio/sdk/entities/id/DidDocument.kt
@@ -1,5 +1,6 @@
 package network.commercio.sdk.entities.id
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.spongycastle.util.encoders.Hex
 import java.security.KeyFactory
@@ -12,18 +13,19 @@ import java.security.spec.PKCS8EncodedKeySpec
  * https://scw-gitlab.zotsell.com/Commercio.network/Cosmos-application/blob/master/Commercio%20Decentralized%20ID%20framework.md
  */
 data class DidDocument(
-    @field:JsonProperty("@context") val context: String,
-    @field:JsonProperty("id") val did: String,
-    @field:JsonProperty("publicKey") val publicKeys: List<DidDocumentPublicKey>,
-    @field:JsonProperty("authentication") val authentication: List<String>,
-    @field: JsonProperty("proof") val proof: DidDocumentProof,
-    @field: JsonProperty("service") val services: List<DidDocumentService>
+    @JsonProperty("@context") val context: String,
+    @JsonProperty("id") val did: String,
+    @JsonProperty("publicKey") val publicKeys: List<DidDocumentPublicKey>,
+    @JsonProperty("authentication") val authentication: List<String>,
+    @JsonProperty("proof") val proof: DidDocumentProof,
+    @JsonProperty("service") val services: List<DidDocumentService>
 ) {
 
     /**
      * Returns the [PublicKey] that should be used as the public encryption key when encrypting data
      * that can later be read only by the owner of this Did Document.
      */
+    @get:JsonIgnore
     val encryptionKey: RSAPublicKey?
         get() {
             // Find the encryption key
