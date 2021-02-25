6e668d841fe40eeb0d656504c360ef2617bea797
valentina <valentina.tessaro.92@gmail.com>
2021-02-25 16:17:29 +0100

    added service field to DidDocumentProofSignatureContent

--- src/main/kotlin/network/commercio/sdk/id/DidDocumentProofSignatureContent.kt
+++ src/main/kotlin/network/commercio/sdk/id/DidDocumentProofSignatureContent.kt
@@ -1,8 +1,10 @@
 package network.commercio.sdk.id
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import network.commercio.sdk.entities.id.DidDocument
 import network.commercio.sdk.entities.id.DidDocumentPublicKey
+import network.commercio.sdk.entities.id.DidDocumentService
 
 /**
  * Represents the contents of the JSON object that should be signed and used inside a [DidDocument]
@@ -11,5 +13,6 @@ import network.commercio.sdk.entities.id.DidDocumentPublicKey
 data class DidDocumentProofSignatureContent(
     @JsonProperty("@context") val context: String,
     @JsonProperty("id") val id: String,
-    @JsonProperty("publicKey") val publicKeys: List<DidDocumentPublicKey>
+    @JsonProperty("publicKey") val publicKeys: List<DidDocumentPublicKey>,
+    @JsonProperty("service") @JsonInclude(JsonInclude.Include.NON_EMPTY) val service: List<DidDocumentService>? = null
 )
